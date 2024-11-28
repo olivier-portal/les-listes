c56c2f35c8153b225a7776a4f3a1fd4d40d71b0c
OLIVIER PORTAL <olivier.portal@laplateforme.io>
2024-11-28 21:08:36 +0100

    JOB12 ok

--- JOB12.py
+++ JOB12.py
@@ -1,11 +1,13 @@
 def liste(list=[8, 24, 27, 48, 2, 16, 9, 7, 84, 91]):
-    index= 0
-    index2=1
-    for index in range(len(list)):
-        if list[index] > list[index2]:
-            list[index], list[index2] = list[index2], list[index]
-            index += 1
-            index2 += 1
+    list_length = 0
+# Iterating through List
+    for _ in list:
+        list_length += 1
+    # print(list_length) Find list length
+    for i in range(list_length): #firts pass
+        for j in range(list_length - 1): #go through all list
+            if list[j] > list[j + 1]: #compare if an element is bigger than the next one
+                list[j], list[j + 1] = list[j + 1], list[j] #reorder list
     return list
         
 ordered_list = liste()
