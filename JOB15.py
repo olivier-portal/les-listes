175590fa2f53c00a193ae48366a9281ae075e607
OLIVIER PORTAL <olivier.portal@laplateforme.io>
2024-11-29 12:08:59 +0100

    Modified JOB14

--- JOB15.py
+++ JOB15.py
@@ -1,6 +1,7 @@
 L = [22.4, 4.0, 16.22, 9.10, 11.00, 12.22, 14.20, 5.20, 17.50]
 
-for i in range(len(L)): L[i] = int(L[i] + 0.5)
+for i in range(len(L)): 
+    L[i] = int(L[i] + 0.5)
 for i in range(len(L)): 
     for j in range(i+1, len(L)): 
         if L[i] > L[j]: L[i], L[j] = L[j], L[i]
