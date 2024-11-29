b0d6514ca40c5b2741926da49e19bb4662dc7a8c
OLIVIER PORTAL <olivier.portal@laplateforme.io>
2024-11-29 12:08:03 +0100

    modified JOB14

--- JOB15.py
+++ JOB15.py
@@ -1,10 +1,9 @@
 L = [22.4, 4.0, 16.22, 9.10, 11.00, 12.22, 14.20, 5.20, 17.50]
 
-# for i in range(len(L)): L[i] = int(L[i] + 0.5)
-# for i in range(len(L)): 
-#     for j in range(i+1, len(L)): 
-#         if L[i] > L[j]: L[i], L[j] = L[j], L[i]
+for i in range(len(L)): L[i] = int(L[i] + 0.5)
+for i in range(len(L)): 
+    for j in range(i+1, len(L)): 
+        if L[i] > L[j]: L[i], L[j] = L[j], L[i]
 
-L = sorted([round(x) for x in L])
 
 print(L)
